682e1d3442714e852353cd42fad1c32474265950
mathesukkj <kemuel.g7363@gmail.com>
2024-05-15 13:29:03 -0300

    fix: spacing in headers

--- internal/blocks/headers.go
+++ internal/blocks/headers.go
@@ -6,7 +6,7 @@ func Headers(headers map[string]string) string {
 	block := fmt.Sprint("headers {\n")
 
 	for key, value := range headers {
-		block += fmt.Sprintf("    %s: %s\n", key, value)
+		block += fmt.Sprintf("  %s: %s\n", key, value)
 	}
 
 	block += "}"
